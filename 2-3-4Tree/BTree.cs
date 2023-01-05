9dbc6dde2be7e464eb5456d0771ae18c8ffc3f69
nicholas.finefter <nfinefter@gmail.com>
2023-01-04 19:03:51 -0800

    Started Insert and Split

--- 2-3-4Tree/BTree.cs
+++ 2-3-4Tree/BTree.cs
@@ -20,144 +20,41 @@ namespace BTree
         {
             if (root == null)
             {
-                root = new Node(key);
+                root = new Node(new List<int> { key});
                 return;
             }
 
             Node current = root;
-            while (true)
+
+            int degree = current.Keys.Count + 1;
+
+            Insert(current, key, -1);
+        }
+
+        private void Insert(Node curr, int key, int index)
+        {
+            if (curr == null)
             {
-                if (current.Key2 == 0)
-                {
-                    // current is a 2-node
-                    if (key < current.Key1)
-                    {
-                        current.Key2 = current.Key1;
-                        current.Key1 = key;
-                    }
-                    else
-                    {
-                        current.Key2 = key;
-                    }
-                    break;
-                }
-                else if (current.Key3 == 0)
-                {
-                    // current is a 3-node
-                    if (key < current.Key1)
-                    {
-                        current.Key3 = current.Key2;
-                        current.Key2 = current.Key1;
-                        current.Key1 = key;
-                    }
-                    else if (key < current.Key2)
-                    {
-                        current.Key3 = current.Key2;
-                        current.Key2 = key;
-                    }
-                    else
-                    {
-                        current.Key3 = key;
-                    }
+                return;
+            }
 
-                    // split the 3-node
-                    Node left = new Node(current.Key1)
-                    {
-                        Left = current.Left,
-                        Middle = current.Middle,
-                        Parent = current
-                    };
-                    Node right = new Node(current.Key3)
-                    {
-                        Left = current.Right,
-                        Parent = current
-                    };
-                    current.Left = left;
-                    current.Middle = null;
-                    current.Right = right;
-                    current.Key1 = current.Key2;
-                    current.Key2 = 0;
-                    current.Key3 = 0;
+            index++;
+            foreach (var item in curr.Keys)
+            {
+                if (item.CompareTo(key) > 0)
+                {
+                    Insert(curr.Children[index], key, index);
                 }
                 else
                 {
-                    // current is a 4-node
-                    if (key < current.Key1)
-                    {
-                        current.Key3 = current.Key2;
-                        current.Key2 = current.Key1;
-                        current.Key1 = key;
-                    }
-                    else if (key < current.Key2)
-                    {
-                        current.Key3 = current.Key2;
-                        current.Key2 = key;
-                    }
-                    else if (key < current.Key3)
-                    {
-                        current.Key3 = key;
-                    }
-                    else
-                    {
-                        key = current.Key3;
-                        current.Key3 = 0;
-                    }
-
-                    // split the 4-node
-                    Node left = new Node(current.Key1);
-
-                    left.Left = current.Left;
-                    left.Middle = current.Middle;
-                    left.Parent = current;
-
-
-                    Node right = new Node(current.Key3);
-
-                    right.Left = current.Right;
-                    right.Parent = current;
-
-                    current.Left = left;
-                    current.Middle = null;
-                    current.Right = right;
-                    current.Key1 = current.Key2;
-                    current.Key2 = 0;
-                    current.Key3 = 0;
-                }
-
-                // move down the tree
-                if (key < current.Key1)
-                {
-                    if (current.Left == null)
-                    {
-                        current.Left = new Node(key);
-                        current.Left.Parent = current;
-                        break;
-                    }
-                    current = current.Left;
-                }
-                else if (current.Key2 == 0 || key < current.Key2)
-                {
-                    if (current.Middle == null)
-                    {
-                        current.Middle = new Node(key);
-                        current.Middle.Parent = current;
-                        break;
-                    }
-                    current = current.Middle;
-                }
-                else
-                {
-                    if (current.Right == null)
-                    {
-                        current.Right = new Node(key);
-                        current.Right.Parent = current;
-                        break;
-                    }
-                    current = current.Right;
+                    //curr.Keys.Insert()
                 }
             }
         }
+        public void Split(Node curr)
+        {
 
+        }
     }
 }
 
---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,17 @@
+using BTree;
+
+namespace UnitTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Insert()
+        {
+            Tree tree = new Tree();
+            for (int i = 10; i < 60; i+= 10)
+            {
+                tree.Insert(i);
+            }
+        }
+    }
+}
