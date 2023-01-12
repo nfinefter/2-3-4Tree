350da4d4d6e4c85ace50fe568efd6288244029d8
nicholas.finefter <nfinefter@gmail.com>
2023-01-11 19:07:39 -0800

    Started Add Function

--- 2-3-4Tree/BTree.cs
+++ 2-3-4Tree/BTree.cs
@@ -10,6 +10,7 @@ namespace BTree
     public class Tree
     {
         private Node root;
+        public const int Degree = 3;
 
         public Tree()
         {
@@ -26,32 +27,57 @@ namespace BTree
 
             Node current = root;
 
-            int degree = current.Keys.Count + 1;
-
-            Insert(current, key, -1);
+            Insert(current, key);
         }
 
-        private void Insert(Node curr, int key, int index)
+        private void Insert(Node curr, int key)
         {
             if (curr == null)
             {
                 return;
             }
 
-            index++;
-            foreach (var item in curr.Keys)
+            if (curr.Children != null)
             {
-                if (item.CompareTo(key) > 0)
+                for (int i = 0; i < curr.Keys.Count; i++)
                 {
-                    Insert(curr.Children[index], key, index);
-                }
-                else
-                {
-                    //curr.Keys.Insert()
+                    if (key < curr.Keys[i])
+                    {
+                        Insert(curr.Children[i], key);
+                     
+                        //checks for split
+                        
+                        return;
+                    }
+                    else if ((key > curr.Keys[i] && curr.Keys.Count == i + 1) || key < curr.Keys[i + 1])
+                    {
+                        Insert(curr.Children[i + 1], key);
+
+                        //checks for split
+
+                        return;
+                    }
                 }
             }
+
+            for (int i = 0; i < curr.Keys.Count; i++)
+            {
+                if (key < curr.Keys[i])
+                {
+                    curr.Keys.Insert(i, key);
+                }
+                else if ((key > curr.Keys[i] && curr.Keys.Count == i + 1) || key < curr.Keys[i + 1])
+                {
+                    curr.Keys.Insert(i + 1, key);
+                }
+                if (curr.Keys.Count == Degree)
+                {
+                    return;
+                }
+            }
+
         }
-        public void Split(Node curr)
+        public void Split(Node curr, int key)
         {
 
         }
--- 2-3-4Tree/Node.cs
+++ 2-3-4Tree/Node.cs
@@ -10,18 +10,6 @@ namespace BTree
 {
     public class Node
     {
-        //public int Key1 { get; set; }
-        //public int Key2 { get; set; }
-        //public int Key3 { get; set; }
-        //public Node Left { get; set; }
-        //public Node Middle { get; set; }
-        //public Node Right { get; set; }
-        //public Node Parent { get; set; }
-
-        //public Node(int key1)
-        //{
-        //    Key1 = key1;
-        //}
 
         public List<int> Keys { get; set; }
         public List<Node> Children { get; set; }
