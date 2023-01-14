1909bdce1c0c93eb42646cabb4891d817e78291e
Nicholas Finefter <nfinefter@gmail.com>
2023-01-14 14:31:02 -0800

    Start Testing Split

--- 2-3-4Tree/BTree.cs
+++ 2-3-4Tree/BTree.cs
@@ -43,13 +43,17 @@ namespace BTree
                 {
                     if (key < curr.Keys[i])
                     {
-                        Insert(curr.Children[i], key);             
+                        Insert(curr.Children[i], key);
+
+                        SplitCheck(curr, i);
 
                         return;
                     }
                     else if ((key > curr.Keys[i] && curr.Keys.Count == i + 1) || key < curr.Keys[i + 1])
                     {
                         Insert(curr.Children[i + 1], key);
+
+                        SplitCheck(curr, i);
 
                         return;
                     }
@@ -60,21 +64,11 @@ namespace BTree
             {
                 if (key < curr.Keys[i])
                 {
-                    curr.Keys.Insert(i, key);
-
-                    if (curr.Keys.Count > 3)
-                    {
-                        Split(curr);
-                    }
+                    curr.Keys.Insert(i, key);             
                 }
                 else if ((key > curr.Keys[i] && curr.Keys.Count == i + 1) || key < curr.Keys[i + 1])
                 {
                     curr.Keys.Insert(i + 1, key);
-
-                    if (curr.Keys.Count > 3)
-                    {
-                        Split(curr);
-                    }
                 }
                 if (curr.Keys.Count == Degree)
                 {
@@ -83,9 +77,42 @@ namespace BTree
             }
 
         }
-        public void Split(Node curr)
+        private void Split(Node curr, int i)
         {
-            //How do i move the last key up?
+            curr.Keys.Add(curr.Children[i].Keys[1]);
+
+            curr.Children[i].Keys.Remove(curr.Children[i].Keys[1]);
+
+            int rightChild = curr.Children[i].Keys[1];
+
+            curr.Children[i].Keys.Remove(curr.Children[i].Keys[1]);
+
+            Node newNode = new Node(new List<int>() { rightChild });
+
+            curr.Children.Insert(i + 1, newNode);
+
+            if (curr.Children[i].Children.Count != 0)
+            {
+                List<Node> newChildren = new List<Node>();
+
+                for (int j = curr.Children[i].Children.Count / 2; j < curr.Children[i].Children.Count;)
+                {
+                    newChildren.Add(curr.Children[i].Children[j]);
+
+                    curr.Children[i].Children.RemoveAt(j);
+                }
+
+                curr.Children[i + 1].Children = newChildren;      
+            }
+            //BugTest Splitting
+            return;
+        }
+        public void SplitCheck(Node curr, int i)
+        {
+            if (curr.Children[i].Keys.Count == Degree)
+            {
+                Split(curr, i);
+            }
         }
     }
 }
